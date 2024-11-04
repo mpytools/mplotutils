7795308113adb3b0ceba7a0ae8752e1cb2e4089f
Mathias Hauser <mathias.hauser@env.ethz.ch>
2024-11-04 11:42:46 +0100

    add version info

--- mplotutils/_deprecate.py
+++ mplotutils/_deprecate.py
@@ -115,7 +115,7 @@ def _module_renamed_warning_init(attr):
     new_module = f"mplotutils._{attr}"
 
     msg = (
-        f"``{old_module}`` is deprecated and has been renamed to ``{new_module}``."
+        f"``{old_module}`` is deprecated and has been renamed to ``{new_module}`` in v0.6.0.."
         f" Note that importing ``{new_module}`` is discuraged. Please use"
         f" functions directly from the  main namespace."
     )
@@ -129,7 +129,7 @@ def _module_renamed_warning(attr, submodule):
     new_module = f"mplotutils._{submodule}"
 
     warnings.warn(
-        f"``{old_module}`` is deprecated and has been renamed to ``{new_module}``."
+        f"``{old_module}`` is deprecated and has been renamed to ``{new_module}`` in v0.6.0."
         f" Note that importing from ``{new_module}`` is discuraged. Please use"
         f" functions directly from the  main namespace, i.e., ``mplotutils.{attr}``",
         FutureWarning,
